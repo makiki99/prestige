9d2212dae0911233ab3e256c399d514e9006a9c8
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-09-12 00:17:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function getGain() {
 
 function getRequirement(id) {
 	if (id === 0) {
-		return Math.floor(Math.pow(1+(Math.pow(0.90, data.prestiges[1])*0.5),data.prestiges[0])*10);
+		return Math.floor(Math.pow(1+(Math.pow(0.90, data.prestiges[1])*0.3),data.prestiges[0])*10);
 	} else if (id === 9) {
 		return Math.floor(Math.pow(10, data.prestiges[id]+1))
 		
