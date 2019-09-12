a144b2d9ff43950d3718a333991a9eace0f24e35
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-09-11 19:30:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function getRequirement(id) {
 		return Math.floor(Math.pow(10, data.prestiges[id]+1))
 		
 	} else {
-		return Math.floor(Math.pow(1+(id*Math.pow(0.95, data.prestiges[id+1])),data.prestiges[id]+1))
+		return Math.floor(Math.pow(1+(id*Math.pow(0.9, data.prestiges[id+1])),data.prestiges[id]+1))
 	}
 }
 
