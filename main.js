b150a9b52e1194f6dade13982abdc33aaf9305f9
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-09-12 21:28:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var data = {
 function getGain() {
 	var gain = 1;
 	for (var i = 0; i < 10; i++) 
-	     gain *= 1+ Math.sqrt(Math.pow(2,data.prestiges[i]));
+	     gain *= 1+Math.pow(data.prestiges[i], 4);
 	     
 	     }
 	return gain;
